6b315d7920dc756cd5376fd3c6c794f75d117ed4
kayteleyn <kayteleyn@gmail.com>
2026-03-25 17:01:23 -0700

    oay

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,1 +1,3 @@
-document.querySelector
+document.querySelector("#rec1button").addEventListener("click", function(e) {
+    document.querySelector("#Recipe1").classList.add("kayteleyn/kayteleyn.github.io/recipe-minisite/recipe1.html")
+});
