ed5ada1258b99a038f46be581198fedfed866511
kayteleyn <kayteleyn@gmail.com>
2026-03-25 17:17:32 -0700

    js alteraton

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,3 +1,4 @@
-document.querySelector("#rec1button").addEventListener("click", function(e) {
-    document.querySelector("#Recipe1").classList.add("kayteleyn/kayteleyn.github.io/recipe-minisite/recipe1.html")
+document.querySelector("#rec1button").addEventListener("click", function () {
+    window.location.href = "recipe1.html";
 });
+
