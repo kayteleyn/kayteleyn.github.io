a82aa72f32cfe87c6e34f7c223ce9b8861347c2f
kayteleyn <kayteleyn@gmail.com>
2026-03-25 17:27:07 -0700

    css and stuff

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -2,3 +2,12 @@ document.querySelector("#rec1button").addEventListener("click", function () {
     window.location.href = "recipe1.html";
 });
 
+document.querySelector("#rec2button").addEventListener("click", function () {
+    window.location.href = "recipe2.html";
+});
+
+document.querySelector("#rec3button").addEventListener("click", function () {
+    window.location.href = "recipe3.html";
+});
+
+
