b3f2afc60f1818b186624cfe518564f6a934c1f6
kayteleyn <kayteleyn@gmail.com>
2026-04-06 22:28:50 -0700

    added stuff for button

---
+++ dark-flower/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let darkModeButton = document.querySelector("#toggleDarkMode");
+
+    darkModeButton.addEventListener("click", function (e) {
+        
+        document.body.classList.toggle("darkMode");
+        
+    });
+
+});
