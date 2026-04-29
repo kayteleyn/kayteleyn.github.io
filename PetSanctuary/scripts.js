6038b1e8d24a270d4a2d158ddcada2e1397ea231
kayteleyn <kayteleyn@gmail.com>
2026-04-28 22:47:23 -0700

    troubleshooting button

--- PetSanctuary/scripts.js
+++ PetSanctuary/scripts.js
@@ -1,3 +1,3 @@
-document.querySelector("#petsite1").addEventListener("click", function () {
-    window.location.href = "Homepage.html";
+document.querySelector("#pet1button").addEventListener("click", function () {
+    window.location.href = "Pet1layout.html";
 });
