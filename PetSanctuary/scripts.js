66bca53f4cb632d5d7e42ab76b8cb1c72dbfd76e
kayteleyn <kayteleyn@gmail.com>
2026-04-28 22:54:20 -0700

    added more buttons and placeholder pages

--- PetSanctuary/scripts.js
+++ PetSanctuary/scripts.js
@@ -1,3 +1,19 @@
 document.querySelector("#pet1button").addEventListener("click", function () {
     window.location.href = "Pet1layout.html";
 });
+
+document.querySelector("#pet2button").addEventListener("click", function () {
+    window.location.href = "Pet2layout.html";
+});
+
+document.querySelector("#pet3button").addEventListener("click", function () {
+    window.location.href = "Pet3layout.html";
+});
+
+document.querySelector("#pet4button").addEventListener("click", function () {
+    window.location.href = "Pet4layout.html";
+});
+
+document.querySelector("#pet5button").addEventListener("click", function () {
+    window.location.href = "Pet5layout.html";
+});
